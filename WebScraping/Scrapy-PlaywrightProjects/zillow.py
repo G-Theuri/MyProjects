b890225a00716662ecc4c93989eca1112ee81c08
G-Theuri <t.gichukierastus@gmail.com>
2024-07-25 06:35:01 +0300

    added

---
+++ WebScraping/Scrapy-PlaywrightProjects/zillow.py
@@ -0,0 +1,21 @@
+
+import scrapy
+from scrapy_playwright.page import PageMethod
+
+
+class ZillowSpider(scrapy.Spider):
+    name = "zill"
+
+    def start_requests(self):
+        yield scrapy.Request("https://www.zillow.com/professionals/real-estate-agent-reviews/san-mateo-county-ca/",
+        meta = dict(
+            playwright = True,
+            playwright_include_page =True,
+            playwright_page_coroutines = [
+                PageMethod('wait_for_selector', 'script#__NEXT_DATA__')
+            ]
+        ))
+        async def parse(self, response):
+            yield{
+                'text': response.text
+            }
