8093d044ff3a0cfdd00826a1d2a4d6fee0ea8729
G-Theuri <t.gichukierastus@gmail.com>
2024-07-31 15:12:37 +0300

    trenshift

--- WebScraping/Scrapy/trendshift.py
+++ WebScraping/Scrapy/trendshift.py
@@ -1,9 +1,15 @@
 import scrapy
+import requests
 
 class TrendshiftSpider(scrapy.Spider):
     name = 'tshift'
-    start_urls = ['https://trendshift.io/repositories/1']
-
+    for i in range(start=11315):
+        url = [f'https://trendshift.io/repositories/{i}']
+        response = requests.get(url)
+        if response.status_code != 200:
+            response = None
+            break
+        
     def parse(self, response):
         for detail in response.css('div.max-w-4xl.mx-auto div'):
             yield{
@@ -11,10 +17,10 @@ class TrendshiftSpider(scrapy.Spider):
                 'github': detail.css('div.mb-4 div div a::attr(href)').get(),
                 'website': detail.css('div.mb-4 div div:nth-child(2) a::attr(href)').get(),
                 'description': detail.css('div.text-sm.text-gray-500 ::text').get(),
+                'language': detail.css('div:nth-child(2)::text').get(),
+                'stars': detail.css('div.mb-2 div div::text').get(),
+                'forks': detail.css('div.mb-2 div div:nth-child(2)::text').get(),                
                 'trendshiftid': detail.css("").get(),
-                'language': detail.css("").get(),
-                'stars': detail.css("").get(),
-                'forks': detail.css("").get(),
                 'rankdate': detail.css("").get(),
                 'rank': detail.css("").get(),
             }
