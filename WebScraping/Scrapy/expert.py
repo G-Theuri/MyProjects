b65e5e6ef736a9eeeb4bc0d1ead7bf53716fdf51
G-Theuri <t.gichukierastus@gmail.com>
2024-08-23 10:37:38 +0300

    expert

--- WebScraping/Scrapy/expert.py
+++ WebScraping/Scrapy/expert.py
@@ -1,6 +1,8 @@
-import scrapy 
-import chompjs
+import scrapy
+import json
+import pandas as pd
 
+alldata = []
 class ExpertSpider(scrapy.Spider):
     name = 'expert'
     start_urls = ['https://www.smartexperts.de/suche/steuerberater/city?from=portal&service=Finanzbuchhaltung,Jahresabschluss']
@@ -11,6 +13,23 @@ class ExpertSpider(scrapy.Spider):
             link = profile.css('div.search-result-container-small a::attr(href)').get()
             page = baseurl+link
             yield response.follow(url=page, callback=self.parse_details)
+
     def parse_details(self, response):
-        javascript = response.css('script[type="application/ld+json"]').get()
-        data = chompjs.parse_js_object(javascript)
+        jsondata = response.css('script[type="application/json"]::text').get()
+        data = json.loads(jsondata)
+        info = {
+            'name': data['office']['name'],
+            'phone': data['office']['phone'],
+            'email': data['office']['email'],
+            'website' : data['office']['website'],
+            'address' : data['office']['address']['city'] + ', ' +
+                         data['office']['address']['street'] + ', ' +
+                         data['office']['address']['country'],
+        }
+        alldata.append(info)
+        df = pd.DataFrame(alldata)
+        df.to_csv('expert.csv', index=False)
+
+        
+        
+    
