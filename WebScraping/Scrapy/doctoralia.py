e4c8ce56ede47cb1cfa6603ce994cb36ca588cb6
G-Theuri <t.gichukierastus@gmail.com>
2024-08-29 21:29:53 +0300

    doctoralia

--- WebScraping/Scrapy/doctoralia.py
+++ WebScraping/Scrapy/doctoralia.py
@@ -1,12 +1,18 @@
-from typing import Any
 import scrapy
-import pandas
-from scrapy.http import Response
+import pandas as pd
 
 class DoctoraliaSpider(scrapy.Spider):
     name = 'doctoralia'
-    for x in range(1, 52):
-        start_urls = f'https://www.doctoralia.com.mx/buscar?q=Fisioterapeuta&loc=Zona%20Metropolitana%20del%20Valle%20de%20M%C3%A9xico&filters%5Bspecializations%5D%5B0%5D=24&page={x}'
+    allowed_domains = ['doctoralia.com.mx']
+    def start_requests(self):
+        for x in range(1, 3):
+            start_url = f'https://www.doctoralia.com.mx/buscar?q=Fisioterapeuta&loc=Zona%20Metropolitana%20del%20Valle%20de%20M%C3%A9xico&filters%5Bspecializations%5D%5B0%5D=24&page={x}'
+            yield scrapy.Request(url=start_url, callback=self.parse)
 
     def parse(self, response):
-        ...
+        for doc in response.css('li.has-cal-active'):
+            data = {
+                'DoctorName':doc.css('div.card.card-shadow-1.mb-1::attr(data-doctor-name)').get(),
+            }
+            name = doc.css('div.card.card-shadow-1.mb-1::attr(data-doctor-name)').get()
+            print(name)
