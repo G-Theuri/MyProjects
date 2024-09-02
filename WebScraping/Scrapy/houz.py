f834976c4d189526ecc43c3c77af4d44753dad5f
G-Theuri <t.gichukierastus@gmail.com>
2024-09-02 22:01:29 +0300

    houz

--- WebScraping/Scrapy/houz.py
+++ WebScraping/Scrapy/houz.py
@@ -1,5 +1,9 @@
-link = "https://www.houzz.com/professionals/general-contractor/san-jose-ca-us-probr0-bo~t_11786~r_5392171"
+import scrapy
+import pandas as pd
+import time
 
+link = "https://www.houzz.com/professionals/general-contractor/san-jose-ca-us-probr0-bo~t_11786~r_5392171?fi=4965"
+first_page = 'https://www.houzz.com/professionals/general-contractor/san-jose-ca-us-probr0-bo~t_11786~r_5392171?fi=0'
 details = {
         
 'BusinessName':"",
