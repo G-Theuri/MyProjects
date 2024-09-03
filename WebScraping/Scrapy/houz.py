709ac356efa83cb503fd6e1084c236a6ec3cc999
G-Theuri <t.gichukierastus@gmail.com>
2024-09-03 21:34:21 +0300

    parsing

--- WebScraping/Scrapy/houz.py
+++ WebScraping/Scrapy/houz.py
@@ -1,16 +1,15 @@
 from typing import Iterable
 import scrapy
 import pandas as pd
-import orjson
+import json
 import time
 
-from scrapy.http import Response
-
 class HouzSpider(scrapy.Spider):
+    k=361
     name = 'houz'
     def start_requests(self):
         page = 1
-        for x in range(0,361,15):
+        for x in range(0,30,15):
             start_url= f'https://www.houzz.com/professionals/general-contractor/san-jose-ca-us-probr0-bo~t_11786~r_5392171?fi={x}'
             print(f'Getting Page: {x}')
             page += 1
@@ -21,17 +20,18 @@ class HouzSpider(scrapy.Spider):
             link = profile.css('a::attr(href)').get()
             yield scrapy.Request(url=link, callback=self.parse_items)
     def parse_items(self, response):
-        script = response.css('script[type="application/json"]::text').get()
-        data = orjson.dumps(script)
-        details = {        
-                    'BusinessName':"",
-                    'Emailaddress':"",
-                    'NumberofReviews':"",
-                    'ReviewRating':"",
-                    'Phonenumber':"",
-                    'Licensenumber':"",
-                    'Website':"",
-                    'Address':"",
-                    'businessownerName':"",
-                    'socialmediaLinks':"",
-        }
+        script = response.xpath('//*[@id="hz-page-content-wrapper"]/script[@type="application/ld+json"]/text()').get()
+        data = json.loads(script)
+        '''details = {        
+                    'BusinessName':data["name"],
+                    #'Emailaddress':data["name"],
+                    'NumberofReviews':data["aggregateRating"]["reviewCount"],
+                    'ReviewRating':data["aggregateRating"]["ratingValue"],
+                    'Phonenumber':data["telephone"],
+                    'Licensenumber':data["name"],
+                    #'Website':data["name"],
+                    'Address':(data["address"]['streetAddress']+data["address"]['addressRegion']+data["address"]['addressCountry']),
+                    #'businessownerName':data["name"],
+                    'socialmediaLinks':data["sameAs"],
+        }'''
+        print(data[''])
