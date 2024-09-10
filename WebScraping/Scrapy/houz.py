d76fc399fd24e35aff09a96ce2aa2ac9f7b62f22
G-Theuri <t.gichukierastus@gmail.com>
2024-09-10 15:45:32 +0300

    Tuesday

--- WebScraping/Scrapy/houz.py
+++ WebScraping/Scrapy/houz.py
@@ -2,15 +2,17 @@ import scrapy
 import pandas as pd
 import json
 import re
+
+
 alldata = []
+
 class HouzSpider(scrapy.Spider):
     name = 'houz'
     def start_requests(self):
-        k=361
         page = 1
-        for x in range(0,30,15):
+        for x in range(0,375,15):
             start_url= f'https://www.houzz.com/professionals/general-contractor/san-jose-ca-us-probr0-bo~t_11786~r_5392171?fi={x}'
-            print(f'Getting Page: {x}')
+            print(f'Getting Page: {page}')
             page += 1
             yield scrapy.Request(url=start_url, callback=self.parse)
 
@@ -18,6 +20,7 @@ class HouzSpider(scrapy.Spider):
         for profile in response.css('ul.hz-pro-search-results.mb0 li'):
             link = profile.css('a::attr(href)').get()
             yield scrapy.Request(url=link, callback=self.parse_items)
+            
     def parse_items(self, response):
         #script = response.css('script[type="application/json"]')
         script = response.xpath('/html/body/div[2]/div[3]/script/text()').get()
@@ -81,4 +84,4 @@ class HouzSpider(scrapy.Spider):
         }
             alldata.append(details)
         df = pd.DataFrame(alldata)
-        df.to_csv('hz.csv', index=False)
+        df.to_csv('houz.csv', index=False)
