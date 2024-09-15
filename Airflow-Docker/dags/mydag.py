8151cd56bd51343fbbf5efa57726276581984b79
G-Theuri <t.gichukierastus@gmail.com>
2024-09-15 20:56:06 +0300

    dag

---
+++ Airflow-Docker/dags/mydag.py
@@ -0,0 +1,28 @@
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+
+
+with DAG(
+    dag_id='my_dag',
+    description='my first basic dag',
+    start_date=datetime(2024, 1, 20, 2),
+    schedule_interval='@daily'
+) as dag:
+    task1 = BashOperator(
+        task_id='first_task',
+        bash_command="echo hello from first task"
+    )
+
+    task2 = BashOperator(
+        task_id='second_task',
+        bash_command="echo hello from second task"
+    )
+
+    task3 = BashOperator(
+        task_id='thrid_task',
+        bash_command="echo hello from third task"
+    )
+
+    task1 >> [task2, task3]
