892a305a95b5ecfa5876214689005ac53ec1266c
G-Theuri <t.gichukierastus@gmail.com>
2024-09-19 07:21:13 +0300

    fire

--- WebScraping/Scrapy/KPL-Project/23-24/table.py
+++ WebScraping/Scrapy/KPL-Project/23-24/table.py
@@ -2,6 +2,7 @@ from curl_cffi import requests as cureq
 import pandas as pd
 import time
 import json
+import os
 
 seasonIDs = {7752:"2014", 9841:"2015", 11265:"2016", 12921:"2017", 15858:"2018", 19876:"2018/2019", 24023:"2019/2020",
              34876:"2020/2021", 38844:"2021/2022", 45686:"2022/2023", 53922:"2023/2024", 65071:"2024/2025"}
@@ -10,18 +11,20 @@ for key in seasonIDs:
     print("Season", seasonIDs[key])
 
 class matches:
-    def __init__(self, seasonID, round):
-        self.seasonID=seasonID
+    def __init__(self, seasonIDs, round):
+        self.seasonIDs=seasonIDs
         self.rounds = round
         self.by_rounds()
         self.by_date()
 
-    def by_rounds(self):
+    def by_rounds(self, seasonID, seasonIDs):
+        os.makedirs(f'Season-{seasonIDs[seasonID]}', exist_ok=True)
         def extract():
             response = cureq.get("f'https://www.sofascore.com/api/v1/unique-tournament/1644/season/{seasonID}/events/round/{round}'",
                                   impersonate="chrome")
             return response
         def transform(response):
+            round_data = []
             games = json.loads(response.text)
             for game in games["events"]:
                 matchdata = {
@@ -53,13 +56,13 @@ class matches:
                     'homeScoreFT' : "-",
                     'awayScoreFT' : "-"
                 }
-                
+            round_data.append(matchdata)
+            return round_data       
 
+        def load(round_data, seasonID, seasonIDs):
+            df = pd.DataFrame(round_data)
+            df.to_csv(f'Season-{seasonIDs[seasonID]}/round-{df["round"][1]}.csv')
 
-            
-        def load():
-            pass
-        pass
     def by_date(self):
         pass
     
