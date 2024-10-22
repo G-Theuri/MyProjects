3dc334d073781ce4191b87306f22e0758e454022
G-Theuri <t.gichukierastus@gmail.com>
2024-10-22 22:19:42 +0300

    brainstorming

--- WebScraping/Upwork/Ahmad/Milestone2/globalviews.py
+++ WebScraping/Upwork/Ahmad/Milestone2/globalviews.py
@@ -1,5 +1,13 @@
+from typing import Iterable
 import scrapy
 import time
 import pandas as pd
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
+
+
+class GlobalViews (scrapy.Spider):
+    name = 'globalviews'
+
+    def start_requests(self):
+        url = 'https://www.globalviews.com/'
