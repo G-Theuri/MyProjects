7cd01bfd60a5bc97251efa83fe08a1122c6a7bf1
G-Theuri <t.gichukierastus@gmail.com>
2024-11-02 23:24:10 +0300

    Great!

--- WebScraping/Upwork/Ahmad/Milestone4/furnitureclassics_scraper.py
+++ WebScraping/Upwork/Ahmad/Milestone4/furnitureclassics_scraper.py
@@ -1,6 +1,4 @@
-from typing import Iterable
 import scrapy
-import time
 from scrapy.crawler import CrawlerProcess
 
 class FurnitureClassics (scrapy.Spider):
@@ -18,7 +16,7 @@ class FurnitureClassics (scrapy.Spider):
             'Storage & Display':['category-1200571','category-1200551','category-1200550', 'category-4632768'],
             'Accessories': ['category-4881094', 'category-4690141', 'category-4648363'],
             'Other': ['category-4899864','category-1200578','category-4632862','category-4830236'],
-            'OPPORTUNITY BUYS':['category-4675572', 'category-4547713']
+            #'OPPORTUNITY BUYS':['category-4675572', 'category-4547713']
         }
         for categoryName in categoryKeys:
             collectionKeys=categoryKeys[categoryName]
@@ -27,6 +25,7 @@ class FurnitureClassics (scrapy.Spider):
                 collectionURL = f'https://supercat.supercatsolutions.com/fc/e/1/products?category_id={collectionID}'
                 yield scrapy.Request(url=collectionURL, callback=self.parse_collection, 
                                      meta={'Category': categoryName, 'data-selection-id':collectionKey})
+                
                 
     def parse_collection(self, response):
         category = response.meta.get('Category', None)
@@ -40,9 +39,26 @@ class FurnitureClassics (scrapy.Spider):
         yield scrapy.Request(url=productURL, callback=self.parse_products,
                              meta={'Category': category, 'Collection':collectionName})
         
+        
     def parse_products(self, response):
         category = response.meta.get('Category', None)
         collection = response.meta.get('Collection', None)
+
+        #Getting Item Details if they are available
+        allDetails = []
+        details = response.css('ul.label-data-list-inline.grid.grid-med li')
+        if details:
+            for detail in details:
+                if detail.css('span.label::text').get() != 'New Intro:':
+                    data = {
+                        'Label': detail.css('span.label::text').get(default=None),
+                        'Data': detail.css('span.data::text').get(default=None)
+                    }
+                    allDetails.append(data)
+        else:
+            allDetails=None
+
+
         yield{
             'Category': category, 
             'Collection': collection,
@@ -50,16 +66,26 @@ class FurnitureClassics (scrapy.Spider):
             'Product Name':response.css('header.item-name h1::text').get(default=None),
             'Product SKU':response.css('header.item-name h2::text').get(default=None),
             'Product Images': {
-                                "Image Pic": response.css('div.item-images-main.carousel-inner div div.item-image-wrapper a::attr(href)').getall(),
-                                "Image PDF": response.css('div.item-images-main.carousel-inner div div.product-image-actions a::attr(href)').getall(),
+                                "Images (jpg)": response.css('div.item-images-main.carousel-inner div div.item-image-wrapper a::attr(href)').getall(),
+                                "Images (PDF}": response.css('div.item-images-main.carousel-inner div div.product-image-actions a::attr(href)').getall(),
                                },
-            'Product Description':response.css('p.story-full::text').get(default=None),
-            #'Product Dimensions',
+            'Product Description':response.css('p.story-full::text').get(),
+            'Product Dimensions/Details':allDetails,
         }
         baseURL ='https://supercat.supercatsolutions.com'
         nextProductLink = response.css('div.right ul.menu-bar-links li a::attr(href)').getall()[-1]
         nextProductLink = baseURL + nextProductLink
         print(nextProductLink)
+        if nextProductLink:
+            yield from self.next_page(nextProductLink,
+                                  meta={'Category':category, 'Collection':collection} )
+        else:
+            nextProductLink = None
+        
+    def next_page(self, nextProductLink, meta):
+        category = meta.get('Category', None)
+        collection = meta.get('Collection', None)
+
         if nextProductLink:
             yield scrapy.Request(url=nextProductLink, callback=self.parse_products,
                              meta={'Category':category, 'Collection':collection} )
@@ -69,3 +95,11 @@ class FurnitureClassics (scrapy.Spider):
         
 
 
+#Setup and run the spider
+process = CrawlerProcess(settings={
+    'FEED_FORMAT' : 'json',
+    'FEED_URI': 'products-data.json', #Output file name. It can be changed accordingly
+    #'LOG_LEVEL': 'INFO' # Set log level to INFO for less verbose output
+})
+process.crawl(FurnitureClassics)
+process.start()
---
+++ WebScraping/Upwork/Ahmad/Milestone5/winners.py
@@ -0,0 +1,67 @@
+from typing import Iterable
+import scrapy
+from scrapy.crawler import CrawlerProcess
+from rich import print as rprint
+
+
+class WinnersOnly(scrapy.Spider):
+    name = 'winners'
+    custom_settings = {
+        'DOWNLOAD_DELAY': 1 # Set the download delay to 1 second
+    }
+    
+
+    def start_requests(self):
+        url = 'https://www.winnersonly.com/m/index.php'
+        yield scrapy.Request(url=url, callback=self.parse_category)
+
+    def parse_category(self, response):
+        categories = response.css('ul.navbar-nav.custom-ul li ').getall()[:-2] #Excludes 'New Arrivals'
+        for category in categories:
+            categoryName = category.css('a::text').get()
+            for collection in category.css('div.dropdown-menu a').getall()[2:]: #[2:] Excludes 'New Arrivals' and 'Collections'
+                baseURL= 'https://www.winnersonly.com/m/'
+                collectionName = collection.css('::text').get()
+                collectionURL = baseURL + category.css('::attr(href)').get()
+                yield scrapy.Request(url=collectionURL, callback=self.parse_products,
+                                      meta={'Category': categoryName, 'Collection':collectionName})
+                
+    def parse_products(self, response):
+        category = response.meta.get('Category', None)
+        collection = response.meta.get('Collection', None)
+        products = response.css('div.row.collRow.justify-content-center.top-m0 ul li')
+        for product in products:
+            baseURL= 'https://www.winnersonly.com/m/'
+            productURL= baseURL + product.css('a::attr(href)').get()
+            yield scrapy.Request(url=productURL, callback=self.parse_data,
+                                 meta={'Category': category, 'Collection':collection})
+            
+    def parse_data(self, response):
+        category = response.meta.get('Category', None)
+        collection = response.meta.get('Collection', None)
+        baseURL= 'https://www.winnersonly.com/m/'
+        product_images = response.css("div#gal1 a::attr(data-zoom-image)").getall()
+
+        yield{
+            'Category':category,
+            'Collection':collection,
+            'URL': response.request.url,
+            'Name': response.css("div.d-flex.flex-column.top-m15 div h4::text").get(),
+            'SKU': response.css("div.d-flex.flex-column.top-m15 div:nth-of-type(3)::text").get(),
+            'Images': [baseURL + product_image for product_image in product_images],
+            'Finish': response.css("h4.top-m15 a:nth-of-type(2)::text").get(),
+            'Dimesions': response.css("div.d-flex.flex-column.top-m15 div:nth-of-type(2)::text").get(),
+            'Details': response.css("div.panel ul li::text").get(),
+            'Documents': {'Document Name': response.css("div.panel.left-m15 p a::text").get(),
+                                  'Document URL': baseURL + response.css("div.panel.left-m15 p a::attr(href)").get()},
+        }
+
+
+#Setup and run the spider
+process = CrawlerProcess(settings={
+    'FEED_FORMAT': 'json',
+    'FEED_URI':'winners.json',#Output file name. It can be changed accordingly
+    #'LOG_LEVEL': 'INFO' # Set log level to INFO for less verbose output
+})
+process.crawl(WinnersOnly)
+process.start()
