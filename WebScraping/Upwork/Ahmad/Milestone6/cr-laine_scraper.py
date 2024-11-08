ec86a45cbe89728d64af937c2efb3059b48449fd
G-Theuri <t.gichukierastus@gmail.com>
2024-11-08 14:39:42 +0300

    Joy

--- WebScraping/Upwork/Ahmad/Milestone6/cr-laine_scraper.py
+++ WebScraping/Upwork/Ahmad/Milestone6/cr-laine_scraper.py
@@ -27,15 +27,13 @@ class LaineSpider(scrapy.Spider):
                 }
                 categories_data.append(categories)
                 
-
-        #follow all category urls and pass the category-name      
+  
         for category in categories_data:
             category_name = category['type']
             category_url = category['url']
             yield scrapy.Request(url=category_url, callback=self.parse_products,
                                  meta={'type':category_name})
             
-            #rprint(f'Menu data appended {category_name} || {category_url}')
     def parse_products(self, response):
 
         all_products = response.css('div.pure-u-1.pure-u-sm-8-24.pure-u-xl-1-4.style_thumbs.text-center')
@@ -53,14 +51,18 @@ class LaineSpider(scrapy.Spider):
                                   })
             
     def parse_data(self, response):
-        rprint(f'Response {response.request.url} received!')
+        rprint(f'Getting data from : {response.request.url}')
+
         #extract image urls
-        '''product_images = response.css('div.pure-g div div.style_thumbs_detail img::attr(zoom)').getall()
+        product_images = response.css('div.pure-g div div.style_thumbs_detail img::attr(zoom)').getall()
         image_urls = []
-        for image in product_images:
-            url = base_url + image
+        if product_images:
+            for image in product_images:
+                url = base_url + image
+                image_urls.append(url)
+        else:
+            url = base_url + response.css('div.zoom-container.hideonmedium img::attr(src)').get()
             image_urls.append(url)
-            rprint(f'Length image_urls: {len(image_urls)}')'''
 
 
         #Extract resources
@@ -68,7 +70,7 @@ class LaineSpider(scrapy.Spider):
         all_resources = []
         for info in resources_info:
             resource ={
-                f"{info.css('::text')}" : f"{base_url + info.css('::attr(href)').get()}",
+                f"{info.css('::text').get().strip()}" : f"{base_url + info.css('::attr(href)').get()}",
                 }
             all_resources.append(resource)
             
@@ -77,22 +79,28 @@ class LaineSpider(scrapy.Spider):
         all_specs = []
         for spec in specs:
             data = {
-                f"{spec.css('span.detailInfoLabel::text').get()}" : f"{spec.css('span:nth-of-type(2)::text').get()}",
+                f"{spec.css('span.detailInfoLabel::text').get().replace(': ', '')}" : f"{spec.css('span:nth-of-type(2)::text').get().strip()}",
             }
             all_specs.append(data)
-        rprint(f'Length all_specs: {len(all_specs)}')
 
         #Extract data under 'SHOWN WITH:'
         materials = response.css('div.pure-g div.pure-u-1.pure-u-lg-1-2.shownwith div')
         all_materials = []
         for material in materials:
-            data = {
-                'Material Name' : material.css('div h4::text').get(),
-                #'Material Thumbnail': base_url + material.css('div img::attr(src)').get(),
-                'Material Thumbname': material.css('div p::text').get(),
-            }
-            all_materials.append(data)
-        
+            material_names= material.css('div[style="margin: auto 0;"] h4')
+            material_thumbnails= material.css('div.pure-u-9-24.pure-u-md-7-24.pure-u-lg-5-24.pure-u-xl-5-24 img')
+            material_thumbnames = material.css('div.pure-u-9-24.pure-u-md-7-24.pure-u-lg-5-24.pure-u-xl-5-24 p')
+            for name, thumbnail, thumbname in zip(material_names, material_thumbnails, material_thumbnames):
+                m_thumbname = thumbname.css('::text').get().strip()
+                raw_url = base_url + thumbnail.css('::attr(src)').get()
+                encoded_url = urllib.parse.quote(raw_url, safe=":/?=&")
+                data = {
+                    'Material Names': name.css('::text').get().strip(),
+                    'Material Thumbnails': encoded_url,
+                    'Material Thumbnames': None if m_thumbname == '' else m_thumbname,
+                }
+                all_materials.append(data)
+    
         #Extract data under related items
         related_items =  response.css('div.relatedProducts')
         all_related_items = {}
@@ -105,26 +113,24 @@ class LaineSpider(scrapy.Spider):
                         'product_collection' : product.css('center a div:nth-of-type(2) span::text').getall()[0].split(' ')[0],
                         'product_sku' : product.css('center a div:nth-of-type(2) span::text').getall()[0].split(' ')[-1],
                         'product_url' : base_url + product.css('center a::attr(href)').get(),
-                        'product_name' : product.css('center a div:nth-of-type(2) span::text').getall()[-1],
+                        'product_name' : ' '.join(product.css('center a div:nth-of-type(2) span::text').getall()[1:]),
                         'thumbnail' : base_url + product.css('center a div img::attr(src)').get(),
                 }
                 all_related_items[title].append(items_info)
-                #
-                #
-            rprint(f'Length related_items: {len(all_related_items)}')
-        
+                
+        #Organize all formated data in one place then load it into a json file
         yield{
             'Type': response.meta.get('type'),
-            'Collection': response.meta.get('type'), 
+            'Collection': response.meta.get('collection'), 
             'Product SKU': response.meta.get('sku'),
             'Product URL': response.request.url,
             'Product Name': response.meta.get('name'),
-            #'Product Images': image_urls,
-            #'Product Comments':
+            'Product Images': image_urls,
+            'Product Comments': response.css('ul.prod-Comments li::text').getall(),
+            'Product Materials': all_materials,
             'Product Resources': all_resources,
             'Product Specifications': all_specs,
-            'Product Materials': all_materials,
-            #'Related Items': related_items,
+            'Related Items': all_related_items,
         }
                 
 
