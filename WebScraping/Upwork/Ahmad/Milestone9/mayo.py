9d4ef6eca5b4a777e2c79d0692dcfe48f36b477c
G-Theuri <t.gichukierastus@gmail.com>
2024-11-13 21:44:26 +0300

    mayo

--- WebScraping/Upwork/Ahmad/Milestone9/mayo.py
+++ WebScraping/Upwork/Ahmad/Milestone9/mayo.py
@@ -1,7 +1,7 @@
 import scrapy
 from scrapy.crawler import CrawlerProcess
 from rich import print as rprint
-import scrapy.resolver
+import urllib.parse
 
 base_url = 'https://www.mayofurniture.com'
 class MayoFurniture(scrapy.Spider):
@@ -32,35 +32,106 @@ class MayoFurniture(scrapy.Spider):
 
     def parse_categories(self, response):
         rprint(f'Getting items on: {response.request.url}')
+
         category = response.meta.get('category')
         type = response.meta.get('type')
         sub_type = response.meta.get('subtype')
 
-        all_products = response.css('div.inner_container div ')
+        all_products = response.css('div.inner_container div')
         for product in all_products:
-            product_url = base_url + product.css('a::attr(href)').get()
+            product_link = product.css('a::attr(href)').get()
+            if product_link:
+                product_url = base_url + product_link
+            else:
+                product_url = None
+                break
             product_name = product.css('span::text').get()
+            rprint(product_url)
             yield scrapy.Request(url=product_url, callback=self.parse_products,
                                  meta={'category': category,'type': type,
                                        'subtype': sub_type, 'productname': product_name,})
     
     def parse_products(self, response):
-        group_measurement = []
-        com_yardage = []
-        product_resources = []
+        rprint(f'Getting data from: {response.request.url}')
+
+        #Get GROUP MEASUREMENTS(gm) & COM YARDAGE (cy) table data
+        gm_cy = {}
+        table_rows = response.css('table.measurementstable tr.itemrow')
+        gm_columns = response.css('table.measurementstable tr.subhead th::text').getall()[1:-6]
+        cy_columns = response.css('table.measurementstable tr.subhead th::text').getall()[-4:]
+        
+        for x in range(0, len(table_rows)):
+                gtable_data = table_rows[x].css('td::text').getall()[2:-6]
+                ctable_data = table_rows[x].css('td::text').getall()[-4:]
+                item_name = table_rows[x].css('td::text').get()
+                gm_cy[item_name]={}
+                gm_cy[item_name]['GROUP MEASUREMENTS']= {}
+                gm_cy[item_name]['COM YARDAGE']= {}
+
+                #populates data from the GROUP MEASUREMENTS table
+                for g_value, g_col in zip(gtable_data, gm_columns):
+                    gm_cy[item_name]['GROUP MEASUREMENTS'][g_col] = g_value
+
+                #populates data from the COM YARDAGE table
+                for c_value, c_col in zip(ctable_data, cy_columns):
+                    gm_cy[item_name]['COM YARDAGE'][c_col] = c_value
+                    
+
+        #Extract image urls
+        image_urls = []
+        image_links = response.css('div#altviews img::attr(src)').getall()
+        if image_links:
+            for link in image_links:
+                url = base_url + urllib.parse.quote(link, safe=":/?=&")
+                image_urls.append(url.replace('/thumbnail/', '/preview/'))
+        else:
+            link = response.css('div.prodpreview_col img#main_preview::attr(src)').get()
+            url = base_url + urllib.parse.quote(link, safe=":/?=&")
+            image_urls.append(url)
+        
+        #Extract resources name and urls
+        resources = response.css('div.pure-u-1.pure-u-lg-6-24 a')
+        all_resources = {}
+        for resource in resources:
+            url = resource.css('::attr(href)').get(default=None)
+            resource_url = base_url + urllib.parse.quote(url, safe=":/?=&") if url else None
+            resource_name = resource.css('div::text').get(default=None)
+            if resource_name:
+                all_resources[resource_name.strip()] = resource_url
+
+        #Extract product dimensions
+        all_dimensions = {}
+        dimensions = response.css('div.pure-u-1.pure-u-lg-6-24 ul:nth-of-type(1) li')
+        for dimension in dimensions:
+            measurement = dimension.css('span:nth-of-type(1)::text').get(default=None)
+            value = dimension.css('span:nth-of-type(2)::text').get(default=None)
+            if measurement:
+                all_dimensions[measurement.strip()] = value
+
+        #Extract product options
+        all_options = {}
+        options = response.css('div.pure-u-1.pure-u-lg-6-24 ul:nth-of-type(2) li')
+        for option in options:
+            name = option.css('span:nth-of-type(1)::text').get(default=None)
+            option_value = option.css('span:nth-of-type(2)::text').get(default=None)
+            if name:
+                all_options[name.strip()] = option_value
+
+
 
         yield{
             'Category':response.meta.get('category'),
             'Type':response.meta.get('type'),
             'Sub-Type': response.meta.get('subtype'),
+            'Product Series': response.css('div.pure-u-1.pure-u-lg-6-24 h2::text').get(),
             'Product URL':response.request.url,
             'Product Name':response.meta.get('productname'),
-            #'Product SKU':response.css(''),
-            #'Product Images':response.css(''),
-            #'Product Resources':response.css(''),
-            #'Product Dimensions':response.css(''),
-            #'Product Images':response.css(''),
-            #'Product Description':response.css(''),
+            'Product SKU':response.request.url.split('/')[-3],
+            'Product Images':image_urls,
+            'Product Dimensions':all_dimensions,
+            'Product Options':all_options,
+            'Product Resources':all_resources,
+            'Measurements Table':gm_cy,
         }
 
 process = CrawlerProcess(settings={
