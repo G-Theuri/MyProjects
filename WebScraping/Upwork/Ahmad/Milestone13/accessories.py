f45290bfea9c96045b9623cc78799a671f08546d
G-Theuri <t.gichukierastus@gmail.com>
2024-11-19 22:39:08 +0300

    bench

--- WebScraping/Upwork/Ahmad/Milestone13/accessories.py
+++ WebScraping/Upwork/Ahmad/Milestone13/accessories.py
@@ -1,4 +1,3 @@
-from typing import Iterable
 import scrapy
 from scrapy.crawler import CrawlerProcess
 from rich import print as rprint
@@ -15,24 +14,14 @@ class Accessories(scrapy.Spider):
         for product in products:
             product_name = product.css('div.row div.col-md-5.align-self-center.section-carousel h2::text').get()
             if product_name == 'Side Table T030 / T030A / T031':
-                name = product_name.replace('T030 / T030A / T031', '')
-                all_info = {
-                    'Category': 'Accessories',
-                    'Sub Category': None,
-                    'Product URL' : response.request.url,
-                    'Product Name' : name,
-                    'Product SKU' : 'T030 / T030A / T031',
-                    #'Product Images':,
-                    #'Product Variations':,                    
-                    'Product Description': None,
-                    'Product Details': None,
-                    'Product Description': None,
-                    'Suites':None
-
-                }
-                #images
-                images = product.css('div.carousel-inner div img::attr(href)')
+                name = product_name.replace(' T030 / T030A / T031', '')
                 
+                #images
+                images = product.css('div#carouselSidetable div div.carousel-item img')
+                image_urls = []
+                for image in images:
+                    url = response.urljoin(image.css('::attr(src)').get())
+                    image_urls.append(url)
                 
                 #swatches
                 swatches = product.css('div.d-flex div')
@@ -47,13 +36,11 @@ class Accessories(scrapy.Spider):
 
                 #manuals
                 manuals = product.css('div.row div.col-md-5.align-self-center.section-carousel div:nth-of-type(3) a')
-                manuals_data = []
+                manuals_data = {}
                 for manual in manuals:
-                    pdfs = {
-                        'Manual Name': manual.css('::attr(href)').get().split('/')[-1].replace('.pdf', '').replace('-', ' ').upper(),
-                        'Manual URL': response.urljoin(manual.css('::attr(href)').get()),
-                    }
-                    manuals_data.append(pdfs)
+                    manual_name =  manual.css('::attr(href)').get().split('/')[-1].replace('.pdf', '').replace('-', ' ').upper()
+                    manual_url = response.urljoin(manual.css('::attr(href)').get())
+                    manuals_data[manual_name] = manual_url
 
                 #table
                 table = product.css('div.row div.col-md-5.align-self-center.section-carousel table tbody tr')
@@ -71,13 +58,77 @@ class Accessories(scrapy.Spider):
                 #rprint(table_data)
                 
 
+                all_info = {
+                    'Category': 'Accessories',
+                    'Sub Category': None,
+                    'Product URL' : response.request.url,
+                    'Product Name' : name,
+                    'Product SKU' : 'T030 / T030A / T031',
+                    'Product Images': image_urls,
+                    'Product Description': None,
+                    'Mechanism': None,
+                    'Product Details': None,
+                    'Product Variations': table_data,                    
+                    'Product Description': None,
+                    'Suites': None,
+                    'Assembly Manual': manuals_data
+                }
+                rprint(all_info)
+
+            else:
+                descr = product_name.split(' ')
+                sku = descr[-1]
+                item_name = product_name.replace(sku, '')
+
+
+                #Images
+                images = product.css('div.carousel-inner div.carousel-item img')
+                image_urls = []
+                for image in images:
+                    url = response.urljoin(image.css('::attr(src)').get())
+                    image_urls.append(url)
+
+                #Dimensions
+                dimension = product.css('div.row div.col-md-5.align-self-center.section-carousel')
+                if product_name == 'Standing Table 0921':
+                    dim = dimension.css('h5::text').get()
+                    feature = None 
+                    manual = response.urljoin(product.css('div:nth-of-type(3) a::attr(href)').get(0))
+                elif product_name =='Laptop Table 0916':
+                    dim = dimension.css('h5::text').get()
+                    feature = dimension.css('div.ml-3.mb-40 li::text').getall()
+                    manual = response.urljoin(product.css('div:nth-of-type(3) a::attr(href)').get(0))
+                elif product_name =='Riser 0034':
+                    dim = None
+                    feature =dimension.css('h5.mb-40.mt-20::text').get()
+                    manual = None
+
+                all_info = {
+                    'Category': 'Accessories',
+                    'Sub Category': None,
+                    'Product URL' : response.request.url,
+                    'Product Name' : item_name,
+                    'Product SKU' : sku,
+                    'Product Images': image_urls,
+                    'Product Description': None,
+                    'Mechanism': None,
+                    'Product Details': {
+                                        'Dimensions': dim,
+                                        'Features': feature,
+                                        },
+                    'Product Variations': None,                    
+                    'Product Description': None,
+                    'Suite': None,
+                    'Assembly Manual': manual
+                }
 
 
 
+                rprint(all_info)
 
 process = CrawlerProcess(settings={
     'FEED_FORMAT': 'json',
-    #'FEED_URI': 'v- products-data.json', #Output file name. It can be changed accordingly
+    #'FEED_URI': 'bm-products-data.json', #Output file name. It can be changed accordingly
     'LOG_LEVEL': 'INFO', #Set log level to INFO for less verbose output
 })
 process.crawl(Accessories)
