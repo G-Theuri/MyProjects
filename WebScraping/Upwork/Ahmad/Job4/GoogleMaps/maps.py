3a38dce00f5e436943c437a160eb27500aa6707b
G-Theuri <t.gichukierastus@gmail.com>
2025-01-14 21:32:49 +0300

    Maps

--- WebScraping/Upwork/Ahmad/Job4/GoogleMaps/maps.py
+++ WebScraping/Upwork/Ahmad/Job4/GoogleMaps/maps.py
@@ -1,35 +1,77 @@
 from playwright.sync_api import sync_playwright
 import itertools, time, os
 
-def get_data(listing):
+def get_data(listing, url, page):
+    #xpaths
+    address_xpath = 'xpath=//button[@data-item-id="address"]//div[contains(@class, "fontBodyMedium")]'
+    website_xpath = 'xpath=//a[@data-item-id="authority"]//div[contains(@class, "fontBodyMedium")]'
+    phone_xpath = 'xpath=//button[contains(@data-item-id, "phone:tel:")]//div[contains(@class, "fontBodyMedium")]'
+
+
     name = listing.get_attribute('aria-label')
+    #ratings = listing.locator('.//span[contains(text(), "reviews")]]/span').first
+    address = page.locator(address_xpath).first.inner_text() if page.locator(address_xpath).first.inner_text() else 'No Address'
+    website = page.locator(website_xpath).first.inner_text() if page.locator(website_xpath).first.inner_text() else 'No Website'
+    phone_number = page.locator(phone_xpath).first.inner_text() if page.locator(phone_xpath).first.inner_text() else 'No Phone Number'
+    #reviews_count = ''
+    #reviews_average = ''
+    
+    coordinates = url.split('@')[-1].split('/')[0]
+    latitude = float(coordinates.split(',')[0])
+    longitude = float(coordinates.split(',')[1])
+    #print(f'Name: {name} | Latitude: {latitude} | Longitude: {longitude}')
+    print(f'Name: {name} | Address: {address} | Website: {website} | Phone: {phone_number}')
 
 
-def get_listings(page):
+def get_listings(page, total_items):
     time.sleep(4)
     page.locator('//div/a[contains(@href, "https://www.google.com/maps/place")]').all()
     listings = page.locator('//div/a[contains(@href, "https://www.google.com/maps/place")]').all()
-    print('Total Listings: ', len(listings))
-    for listing in listings:
+
+    print('\nTotal Listings found: ', len(listings))
+    print('\nTotal Listings to be scraped: ', len(listings[0: int(total_items)]))
+
+    for listing in listings[0: int(total_items)]:
         listing.click()
         time.sleep(3)
         page.wait_for_load_state('networkidle')
-
-        name = listing.get_attribute('aria-label')
-        ratings = listing.locator('./span[contains(text(), "reviews")]]/span').first
-        print(name)
+        url = page.url
+        get_data(listing, url, page)
+        time.sleep(2)
 
 def scroll_page(page, total_items):
     feed = page.locator('[role="feed"]')
     feed.hover()
-    num_scrolls = int(int(total_items) / 2) + 2
-    for _ in range(num_scrolls):
+    listings = 0
+    MAX_SCROLL_RETRIES = 4
+    retry_attempts = 0
+
+    while listings < int(total_items):
+
         feed.evaluate('element => element.scrollTop += element.clientHeight')
+        current_listings = len(page.locator('//div/a[contains(@href, "https://www.google.com/maps/place")]').all())
         time.sleep(3)
+
+        if listings == current_listings:
+            retry_attempts += 1 
+            if retry_attempts >= MAX_SCROLL_RETRIES:
+                print(f'End of page! Total listings found: {listings}', end='')
+                break
+            else:
+                continue #Try scrolling again 
+        else:
+            listings = current_listings
+            retry_attempts = 0
     
 def main():
     search_term = input('Enter Your Search Term: ')
-    total_items = input('Enter the number of listings you wish to scrape: ')
+    while True:
+        try:
+            total_items = int(input('Enter the number of listings you wish to scrape: '))
+            break
+        except:
+            print("That's not a valid integer. Please try again.")
+
     with sync_playwright() as p:
         browser = p.chromium.launch(headless=False)
         page = browser.new_page()
@@ -42,7 +84,7 @@ def main():
         page.wait_for_load_state('networkidle')
 
         scroll_page(page, total_items)
-        get_listings(page)
+        get_listings(page, total_items)
 
         time.sleep(5)
         browser.close()
--- parker.py
+++ parker.py
@@ -1,16 +1,8 @@
 import time
 import itertools
 
-total = 10
-spinner_chars = ['|', '/', '-', '\\']
-spinner = itertools.cycle(spinner_chars)
+url = 'https://www.google.com/maps/place/The+Arcadia+Barbers/@-1.2196278,36.8600582,14z/data=!4m10!1m2!2m1!1sbarber,+kasarani!3m6!1s0x182f155e60ff44f3:0xde8f862b18bb1f0d!8m2!3d-1.2196278!4d36.8961071!15sChBiYXJiZXIsIGthc2FyYW5pWhEiD2JhcmJlciBrYXNhcmFuaZIBC2JhcmJlcl9zaG9w4AEA!16s%2Fg%2F11t_zscf7c?entry=ttu&g_ep=EgoyMDI1MDEwOC4wIKXMDSoASAFQAw%3D%3D'
 
-for x in range(1, total+1):
-    if x != total:
-        spinner_char = next(spinner)
-    else:
-        spinner_char = ''
-    print(f"\rPage {x} of {total} {spinner_char}", end='')
-
-    time.sleep(2)
-print('\nDone!')
+coordinates = url.split('@')[-1].split('/')[0]
+print(coordinates)
+print(float(coordinates.split(',')[1]))
