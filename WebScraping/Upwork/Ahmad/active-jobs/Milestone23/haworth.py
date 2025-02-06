9d274ec6087e697059dd32af2773131dad350536
G-Theuri <t.gichukierastus@gmail.com>
2025-02-06 22:37:54 +0300

    haworth

--- WebScraping/Upwork/Ahmad/active-jobs/Milestone23/haworth.py
+++ WebScraping/Upwork/Ahmad/active-jobs/Milestone23/haworth.py
@@ -7,50 +7,68 @@ import pandas as pd
 import time, os
 from rich import print
 
-def get_data():
-    pass
+def get_data(driver, url):
+    print(f'Getting data from {url}')
+
+def additional_search(driver, term):
+        print()
+        # try:
+        #     listings = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.XPATH, '//ul[@class="listing-cards is-card-view grid-x grid-margin-x grid-margin-y"]/li/a')))
+        #     print(len(listings))
+        # except Exception as e:
+        #     listings = None
+
+        # if listings:
+        #     names = []
+        #     for listing in listings[:1]:
+        #         name = listing.find_element(By.XPATH, '//div[@class="listing-card__details"]/h2').text
+        #         print(name)
+        pass
 
 def main():
 
     filepath = 'C:/Users/TG/Downloads/WebScrape-Content-Template.xlsx'
+    search_terms = ["IMPROV Sled Base", "X99 Upholstered Guest Chair", "IMPROV Sled Base", "Conover - Pull-Out, Narrow Arm", "IMPROV", "IMPROV", "IMPROV H.E. Stool", "IMPROV H.E.", "Pip Personal Laptop Table"]
     
     options = uc.ChromeOptions()
     options.add_argument('--disable-popup-blocking')
     driver = uc.Chrome(options)
     driver.maximize_window()
 
-    driver.get('https://www.haworth.com/eu/en/search.html?q=')
+    #driver.get('https://www.haworth.com/eu/en/search.html?q=')
+    driver.get('https://store.haworth.com/search?')
 
-    try: 
-        WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, '//*[@id="CybotCookiebotDialogBodyButtonDecline"]'))).click() 
-    except Exception: 
+    try:
+        WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, '//div[@id="ltkpopup-close-button"]/button[@class="ltkpopup-close"]'))).click() 
+    except Exception:
         pass
     
-    search_terms = ["IMPROV Sled Base", "X99 Upholstered Guest Chair", "IMPROV Sled Base", "Conover - Pull-Out, Narrow Arm", "IMPROV", "IMPROV", "IMPROV H.E. Stool", "IMPROV H.E.", "Pip Personal Laptop Table"]
-
-    searchbar = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//div/form/input[@aria-label="search"]')))
-
+    searchbar = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//div/form/input[@class="Search__Input"]')))
     for term in search_terms:
         pass
         searchbar.clear()
         searchbar.send_keys(term)
-        searchbar.send_keys(Keys.ENTER)
-        time.sleep(5)
+        time.sleep(3)
 
         try:
-            listings = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.XPATH, '//ul[@class="listing-cards is-card-view grid-x grid-margin-x grid-margin-y"]/li/a')))
-            print(len(listings))
-        except Exception as e:
-            listings = None
+            suggestions = WebDriverWait(driver, 5).until(EC.presence_of_all_elements_located((By.XPATH, '//ul[@class="Grid Grid--xl"]/li')))
+            #print(len(suggestions))
+            if suggestions:
+                for suggestion in suggestions[:1]:
+                    url = WebDriverWait(suggestion, 2).until(EC.presence_of_element_located((By.XPATH, './div/div/div/a')))
+                    url = url.get_attribute('href')
+                    get_data(driver, url)
 
-        if listings:
-            names = []
-            for listing in listings:
-                name = listing.find_element(By.XPATH, '//div[@class="listing-card__details"]/h2').text
-                print(name)
+        except:
+            additional_search(driver, term)
+
         
+        #searchbar.send_keys(Keys.ENTER)
+        time.sleep(5)
 
-    time.sleep(3000)
+
+
+    time.sleep(5)
 
     driver.quit()
 
