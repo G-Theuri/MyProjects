ae2bae672b43c529bfa71e9b0c9f8358b0e743f8
G-Theuri <t.gichukierastus@gmail.com>
2025-02-12 00:04:00 +0300

    stevesilver

--- WebScraping/Upwork/Ahmad/active-jobs/Milestone24/stevesilver_scraper.py
+++ WebScraping/Upwork/Ahmad/active-jobs/Milestone24/stevesilver_scraper.py
@@ -1,10 +1,11 @@
 import scrapy
 from scrapy.crawler import CrawlerProcess
 import pandas as pd
-from rich import print as rprint
+from rich import print
+import time
 
 
-info = {}
+
 class SteveSilver(scrapy.Spider):
     name= 'steve-silver'
 
@@ -13,38 +14,68 @@ class SteveSilver(scrapy.Spider):
         yield scrapy.Request(url, callback=self.parse_categories)
 
     def parse_categories(self, response):
-        categories = response.css('ul#menu-main-menu li')
-        for category in categories[1:]:
+        categories = response.xpath('//ul[@id= "menu-main-menu"]/li')
+        for category in categories[1:-2]:
             category_name = category.css('a span.menu-text::text').get()
-            info['Category 1'] = category_name
-            submenus = category.css('ul.sub-menu li')
+            #rprint(category_name)
+            submenus = category.xpath('./ul[@class="sub-menu"]/li')
             
             for submenu in submenus:
-                submenu_name = submenu.css('a.span::text').get()
-                info['Category 2'] = submenu_name
-                sub_submenus = submenu.css('ul.sub-menu li')
+                submenu_name = submenu.xpath('./a/span/text()').get()
+                sub_submenus = submenu.xpath('./ul[@class="sub-menu"]/li')
 
                 if sub_submenus:
                     for sub_submenu in sub_submenus:
-                        sub_submenu_name = sub_submenu.css('a.span::text').get()
-                        info['Category 3'] = sub_submenu_name
-                        tertiary_submenus = sub_submenu.css('ul.submenu li')
+                        sub_submenu_name = sub_submenu.xpath('./a/span/text()').get()
+                        tertiary_submenus = sub_submenu.xpath('./ul[@class="sub-menu"]/li')
 
                         if tertiary_submenus:
                             for tertiary_submenu in tertiary_submenus:
-                                tertiary_submenu_name = tertiary_submenu.css('a.span::text').get()
+                                tertiary_submenu_name = tertiary_submenu.xpath('./a/span/text()').get()
                                 tertiary_submenu_url = tertiary_submenu.css('a::attr(href)').get()
-                                info['Category 4'] = tertiary_submenu_name
-                                info['Category 4 URL'] = tertiary_submenu_url
-                                rprint(info)    
+                                #rprint(f'{category_name} : {submenu_name} : {sub_submenu_name} : {tertiary_submenu_name} : {tertiary_submenu_url}')
+                                time.sleep(5)
+                                yield scrapy.Request(url= tertiary_submenu_url, callback=self.get_products,
+                                                meta={'Category 1':category_name, 'Category 2':submenu_name, 
+                                                      'Category 3':sub_submenu_name, 'Category 4':tertiary_submenu_name})
+
 
                         else:
                             sub_submenu_url = sub_submenu.css('a::attr(href)').get()
-                            info['Category 3 URL'] = sub_submenu_url
-
+                            #rprint(f'{category_name} : {submenu_name} : {sub_submenu_name} : {sub_submenu_url}')
+                            time.sleep(7)
+                            yield scrapy.Request(url= sub_submenu_url, callback=self.get_products,
+                                                meta={'Category 1':category_name, 'Category 2':submenu_name, 
+                                                      'Category 3':sub_submenu_name})
                 else:
                     submenu_url = submenu.css('a::attr(href)').get()
-                    info['Category 2 URL'] = submenu_url
+                    #rprint(f'{category_name} : {submenu_name}: {submenu_url}')
+                    time.sleep(6)
+                    yield scrapy.Request(url= submenu_url, callback=self.get_products,
+                                         meta={'Category 1':category_name, 'Category 2':submenu_name})
+
+    def get_products(self, response):
+        info = {
+            'Category 1':response.meta.get('Category 1'),
+            'Category 2':response.meta.get('Category 2'),
+            'Category 3':response.meta.get('Category 3'),
+            'Category 4':response.meta.get('Category 4'),
+            'URL':  response.url,
+        }
+        #rprint(info)
+        try:
+            products = response.css('div.woocommerce-container ul li')
+            print(f'{info['URL']}: {len(products)}')
+             
+        except Exception as e:
+            listings = response.xpath('//div[@class="post-content"]/div')
+            products = []
+            for listing in listings:
+                url = listing.xpath('.//div[@class="woocommerce columns-4"]/ul/li/div/@href').get()
+                products.append(url)
+            print(f'{info['URL']}: {len(products)}')
+
+
 
 
 
