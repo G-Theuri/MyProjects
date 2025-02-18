559cafe5ad12891f051613243e10c46bc71a4d8f
G-Theuri <t.gichukierastus@gmail.com>
2025-02-18 13:35:38 +0300

    leathercraft

--- WebScraping/Upwork/Ahmad/active-jobs/Milestone25/grainger.py
+++ WebScraping/Upwork/Ahmad/active-jobs/Milestone25/grainger.py
@@ -8,6 +8,108 @@ import time, os
 from rich import print
 import pandas as pd
 
+def parse_documents(documents, df, index):
+    manual_key = ['Manual', 'Parts Diagram', 'OIPM', 'Assembly Instructions']
+    brochure_key = ['Sell sheet', 'Selection Guide']
+    specssheet_key = ['Specification Sheet', 'Technical Data Sheet', 'Tech Sheet']
+    if documents:
+        for document in documents:
+            doc_name = document.text
+            doc_url = document.get_attribute('href')
+
+            if any(br_key.lower() in doc_name.lower() for br_key in brochure_key) :
+                df.at[index, 'Brochure (pdf)'] = doc_url
+
+            elif any(m_key.lower() in doc_name.lower() for m_key in manual_key ):
+                df.at[index, 'Manual/IFU (pdf)'] = doc_url
+
+            elif any(ss_key.lower()  in doc_name.lower() for ss_key in specssheet_key):
+                df.at[index, 'Specification Sheet (pdf)'] = doc_url
+
+            else:
+                pass
+    return df
+
+def parse_details(details, df, index):
+    amp_keys = ['Amperage', 'current', 'Amps AC', 'Amps']
+    volt_keys = ['Voltage', 'Operating Voltage', 'Rated Voltage', 'Output Amplitude']
+    watt_keys = ['Power Output', 'Power Consumption', 'Cooking Wattage', 'Wattage', 'Watts']
+    phase_keys = ['Phase']
+    hertz_keys = ['Frequency', 'Hz']
+    plug_type_keys = ['Plug Type']
+    depth_keys = ['Overall Depth', 'Depth',  'Housing Sz (H x W x D)', 'Handle Length', 'Overall Length', 'Table Length', 'Body Depth', 'Tool Length', 'Outside Depth', 'Base Length']
+    height_keys = ['Overall Height', 'Width', 'Width/Diameter', 'Housing Sz (H x W x D)', 'Overall Fixed Height', 'Table Height', 'Body Height', 'Outside Height', 'Stowed Height', 'Overall Height - Maximum']
+    width_keys = ['Overall Width/ Height/ Housing Sz (H x W x D)/ Table Width/Body Width/ Outside Width/ Base Width']
+    ada_compliant_keys = ['ADA Compliant', 'ADA Compliance']
+    antimicrobial_coating_keys=	['Antimicrobial', 'Features']
+
+    if details:
+        for detail in details:
+            key = WebDriverWait(detail, 2).until(EC.presence_of_element_located((By.XPATH, './dt')))
+            value = WebDriverWait(detail, 2).until(EC.presence_of_element_located((By.XPATH, './dd')))
+
+            if any(amp_key.lower() in key.lower() for amp_key in amp_keys) :
+                df.at[index, 'amps'] = value
+
+            elif any(volt_key.lower() in key.lower() for volt_key in volt_keys) :
+                df.at[index, 'volts'] = value
+            
+
+            elif any(watt_key.lower() in key.lower() for watt_key in watt_keys) :
+                df.at[index, 'watts'] = value
+
+            elif any(phase_key.lower() in key.lower() for phase_key in phase_keys) :
+                df.at[index, 'phase'] = value
+            
+            
+            elif any(hertz_key.lower() in key.lower() for hertz_key in hertz_keys) :
+                df.at[index, 'hertz'] = value
+
+            elif any(plug_type_key.lower() in key.lower() for plug_type_key in plug_type_keys) :
+                df.at[index, 'plug_type'] = value
+            
+            
+            elif any(depth_key.lower() in key.lower() for depth_key in depth_keys) :
+                if 'x' in value:
+                    value = value.split('x')[-1]
+                    df.at[index, 'depth'] = value
+                else:
+                    df.at[index, 'depth'] = value
+
+            elif any(height_key.lower() in key.lower() for height_key in height_keys) :
+                if 'x' in value:
+                    value = value.split('x')[0]
+                    df.at[index, 'height'] = value
+                else:
+                    df.at[index, 'height'] = value
+           
+            
+            elif any(width_key.lower() in key.lower() for width_key in width_keys) :
+                if 'x' in value:
+                    value = value.split('x')[1]
+                    df.at[index, 'width'] = value
+                else:
+                    df.at[index, 'width'] = value
+            
+            
+            elif any(ada_key.lower() in key.lower() for ada_key in ada_compliant_keys) :
+                if value =='No' or value == 'Non-ADA Compliant':
+                    df.at[index, 'ada compliant (Y/N)'] = 'N'
+            
+            
+            elif any(ac_key.lower() in key.lower() for ac_key in antimicrobial_coating_keys) :
+                if value.lower == 'no':
+                    df.at[index, 'antimicrobial coating (Y/N)'] = 'N'
+                elif value.lower == 'antimicrobial' or value.lower == 'yes':
+                    df.at[index, 'antimicrobial coating (Y/N)'] = 'Y'
+
+            
+            else:
+                pass             
+        
+    return df
+
+
 def get_elements(driver, xpath, multiple, timeout=4):
     try:
         if multiple:
@@ -17,7 +119,7 @@ def get_elements(driver, xpath, multiple, timeout=4):
     except TimeoutException:
         return [] if multiple else None
 
-def search_items(driver, model_number):
+def search_items(driver, model_number, df, row, index):
     searchbar = driver.find_element(By.XPATH, '//div/input[@aria-label="Search Query"]')
     max_retries = 2
     retry_delay = 1
@@ -29,7 +131,7 @@ def search_items(driver, model_number):
             try:
                 searchbar.clear()
                 searchbar.send_keys(model_number) #Type in the Model-Number 
-                searchbar.send_keys(Keys.RETURN) #Hit ENTER
+                searchbar.send_keys(Keys.RETURN) #Hit ENTER 
                 time.sleep(3)
 
                 item_url = driver.current_url
@@ -38,29 +140,41 @@ def search_items(driver, model_number):
                 image = get_elements(driver, '//div[@data-testid="product-image-to-zoom"]/img', multiple=False)
                 price = get_elements(driver, '//div/span[@class="HANkB IuSbF N5ad3 xqCG3 a0SF- _4TUUj"]', multiple=False)
                 shipping_weight = get_elements(driver, '//div[@data-testid="shipping-weight"]/strong', multiple=False)
+
                 documents = get_elements(driver, '//div[@data-testid="product-documents-list"]/div/a', multiple=True)
+                if documents:
+                    df = parse_documents(documents, df, index)
+
                 details = get_elements(driver, '//div/dl[@data-testid="product-techs"]/div', multiple=True)
+                if details:
+                    df = parse_details(details, df, index)
 
                 # if sku.text == model_number.strip():
                 #     #print(sku.text, item_url)
                 info = {
-                        'SKU':sku.text,
-                        'Image':image.get_attribute('src'),
-                        'Price':price.text,
-                        'Shipping Weight':shipping_weight.text,
+                        'SKU':sku.text if sku else '',
+                        'Image':image.get_attribute('src') if image else '',
+                        'Price':price.text if price else '',
+                        'Shipping Weight':shipping_weight.text if shipping_weight else '',
                     }
-                print(info)
+                if info:
+                    df.at[index, 'Product URL'] = item_url
+                    df.at[index, 'Product Image (jpg)'] = info['Image']
+                    df.at[index, 'Product Image'] = info['Image']
+                    df.at[index, 'Shipping Weight'] = info['Shipping Weight']
+
                 success=True
                 driver.back()
-            except:
+            except Exception as e:
                 retries += 1
                 if retries < max_retries:
                     driver.get('https://www.grainger.com/')
                     time.sleep(retry_delay)
                 else:
-                    print(f'[yellow]{model_number}[/yellow] [red]Not found![/red]')
+                    print(f'[yellow]{model_number}[/yellow] [red]Not found![/red] >>>>>>>>> error: {e}')
                     break  # Exit loop after retries are exhausted
-    except:
+    except Exception as e:
+        print(f'An error occured: {e}')
         pass
 
 
@@ -86,7 +200,7 @@ def main():
         for index, row in df.tail(5).iterrows():
             model_number = row['mfr number']
             print(model_number)
-            search_items(driver, model_number)
+            search_items(driver, model_number, df, row, index)
             time.sleep(2)
 
     except Exception as e:
--- WebScraping/Upwork/Ahmad/complete-Jobs/Milestones/Milestone11/leathercraft_scraper.py
+++ WebScraping/Upwork/Ahmad/complete-Jobs/Milestones/Milestone11/leathercraft_scraper.py
@@ -12,6 +12,7 @@ header = {
     "sec-ch-ua":'"Chromium";v="130", "Google Chrome";v="130", "Not?A_Brand";v="99"',
     "user-agent":'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36',    
     }
+visited_urls = set()
 
 def start_requests(url, header):
     response = cureq.get(url=url, impersonate='chrome', headers=header)
@@ -32,16 +33,16 @@ def start_requests(url, header):
                 response = cureq.get(url=subtype_url, impersonate='chrome', headers=header)
                 time.sleep(2)
                 info = {'Category': type_name, 'Collection': subtype_name} #Type and Sub-Type denoted as Category and Collection for Uniformity
-                extract(response, info, header)
+                parse_products(response, info, header)
                 continue
 
         else:
             response = cureq.get(url=type_url, impersonate='chrome', headers=header)
             time.sleep(2)
             info = {'Category': type_name, 'Collection': None} #Type and Sub-Type denoted as Category and Collection for Uniformity with previous outputs
-            extract(response, info, header)
+            parse_products(response, info, header)
 
-def extract(response, info, header):
+def parse_products(response, info, header):
     rprint(f'Current Products Page: {response.url}')
     tree = html.fromstring(response.content)
     next_page = tree.xpath('/html/body/div/div/nav/a[contains(text(), "Next Page")]/@href')
@@ -50,81 +51,108 @@ def extract(response, info, header):
     if not next_page:
         for product in products:
             product_url = product.xpath('./@href')[0]
-            rprint(f'Getting data from : {product_url}')
+            #rprint(f'Getting data from : {product_url}')
             time.sleep(0.4)
             response = cureq.get(url=product_url, impersonate='chrome', headers=header)
             transform(response, info)
+            visited_urls.add(product_url)
             time.sleep(1.4)
     else:
         for product in products:
             product_url = product.xpath('./@href')[0]
-            rprint(f'Getting data from : {product_url}')
+            #rprint(f'Getting data from : {product_url}')
             time.sleep(0.4)
             response = cureq.get(url=product_url, impersonate='chrome', headers=header)
             time.sleep(1.7)
 
             transform(response, info)
+            visited_urls.add(product_url)
+
         get_next_page(next_page[0], info, header)
 
 def get_next_page(next_page, info, header):
     response = cureq.get(url=next_page, impersonate='chrome', headers=header)
     time.sleep(2.1)
-    extract(response, info, header)
+    parse_products(response, info, header)
 
     
 def transform(response, info):
-    tree = html.fromstring(response.content)
+    if response.url not in visited_urls:
+        rprint(f'Getting data from : {response.url}')
+        time.sleep(2)
+        tree = html.fromstring(response.content)
 
-    #Extract Product SKU
-    title = tree.xpath('/html/body/div/article/div[1]/h2/text()')
-    if title:
-        sku = title[0].split(' ')[0]
-        name = title[0].replace(sku, '').replace('- QS Frame', '').strip()
-    else:
-        sku = None
-
-    #Extract Dimensions
-    dimensions = tree.xpath('/html/body/div/article/div[1]/div[3]/p')[0].text_content()
-    dim_values = dimensions.replace('\n\t\t\t\t\t\t\t', '%').replace(' \n\t\t\t\t\t\t\t', '%').replace('\t\t\t\t\t\t\t\t\t\t\t\t\t\t', '%')\
-        .replace('\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t', '%').replace(' \t\t\t\t\t\t\t\t\t\t\t\t\t', '%')#.replace(' ', '')
-    all_dimensions= {}
-    dim_items = dim_values.split('%')
-    for item in dim_items[1:-1]:
-        if item != '':
-            item_data = item.split(':')
-            all_dimensions[item_data[0]] = item_data[1]
-
-
-    #Extract Pricing Information
-    fields = ["Yardage", "CTN WT", "STD Finish", "Exposed Wood", "STD Trim", "STD Seat Cushion", "STD Back Cushion"]
-    pricing_data = {}
-    for field in fields:
-        xpath_expr = f'//div[contains(@class, "product-pricing")]/p/strong[contains(text(), "{field}:")]/following-sibling::text()'
-        value = tree.xpath(xpath_expr)
-        if value:
-            pricing_data[field] = value[0].strip()
+        #Extract Product SKU
+        title = tree.xpath('/html/body/div/article/div[1]/h2/text()')
+        if title:
+            sku = title[0].split(' ')[0]
+            name = title[0].replace(sku, '').replace('- QS Frame', '').strip()
         else:
-            pricing_data[field] = None
+            sku = None
+
+        #Extract Dimensions
+        dimensions = tree.xpath('/html/body/div/article/div[1]/div[3]/p')[0].text_content()
+        dim_values = dimensions.replace('\n\t\t\t\t\t\t\t', '%').replace(' \n\t\t\t\t\t\t\t', '%').replace('\t\t\t\t\t\t\t\t\t\t\t\t\t\t', '%')\
+            .replace('\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t', '%').replace(' \t\t\t\t\t\t\t\t\t\t\t\t\t', '%')#.replace(' ', '')
+        all_dimensions= {}
+        dim_items = dim_values.split('%')
+        for item in dim_items[1:-1]:
+            if item != '':
+                item_data = item.split(':')
+                all_dimensions[item_data[0]] = item_data[1]
+        
+        #Extract Suites Information
+        suites = tree.xpath('/html/body/div/article/div[1]/nav[2]/ul/li/a/text()')
+        suite_urls = tree.xpath('/html/body/div/article/div[1]/nav[2]/ul/li/a/@href')
+        if suite_urls:
+            get_suites(info, suite_urls)
+
+        #Extract Pricing Information
+        keys = tree.xpath('//div[contains(@class, "product-pricing")]//p//strong/text()')
+        values = tree.xpath('//div[contains(@class, "product-pricing")]//p//strong/following-sibling::text()')
+
+        pricing_data = {}
+        for k, val in zip(keys, values):
+            key = k.strip(':').strip()
+            value = val.strip()
+            pricing_data[key] = value
 
 
-    all_data = {
-        **info, #Type and Sub-Type denoted as Category and Sub-Category for Uniformity
-        'Product URL': response.url,
-        'Product Name': name,
-        'Product SKU': sku,
-        'Product Images': tree.xpath('/html/body/div/article/div[1]/figure/img/@src'),
-        'Product Description': tree.xpath('/html/body/div/article/div[1]/div[2]/p/text()'),
-        'Product Dimesions':all_dimensions,
-        'Suite': tree.xpath('/html/body/div/article/div[1]/nav[2]/ul/li/a/text()'),
-        'Pricing Information': pricing_data,
-    }
+        # fields = ["Yardage", "CTN WT", "STD Finish", "Exposed Wood", "STD Trim", "STD Seat Cushion", "STD Back Cushion"]
+        # pricing_data = {}
+        # for field in fields:
+        #     xpath_expr = f'//div[contains(@class, "product-pricing")]/p/strong[contains(text(), "{field}:")]/following-sibling::text()'
+        #     value = tree.xpath(xpath_expr)
+        #     if value:
+        #         pricing_data[field] = value[0].strip()
+        #     else:
+        #         pricing_data[field] = None
 
-    load(all_data)
-    rprint(all_data)
+
+        all_data = {
+            **info, #Type and Sub-Type denoted as Category and Sub-Category for Uniformity
+            'Product URL': response.url,
+            'Product Name': name,
+            'Product SKU': sku,
+            'Product Images': tree.xpath('/html/body/div/article/div[1]/figure/img/@src'),
+            'Product Description': tree.xpath('/html/body/div/article/div[1]/div[2]/p/text()'),
+            'Schemtic PDF':tree.xpath('/html/body/div/article/div[1]/div[2]/p/a[contains(@class, "pdf-link")]/@href'),
+            'Product Dimesions':all_dimensions,
+            'Suite': suites,
+            'Pricing Information': pricing_data,
+        }
+
+        load(all_data)
+        #rprint(all_data)
+
+def get_suites(info, suite_urls):
+    for suite_url in suite_urls:
+        response = cureq.get(url=suite_url, impersonate='chrome', headers=header)
+        transform(response, info)
 
 
 def load(all_data):
-    file_name = 'products-data.json' #Output file name
+    file_name = 'new-products-data.json' #Output file name
 
     #Appends data into the output file
     if os.path.exists(file_name):
