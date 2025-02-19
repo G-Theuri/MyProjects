c7e653837058d813fc6b746b55e17dae8c3e617f
G-Theuri <t.gichukierastus@gmail.com>
2025-02-19 12:30:51 +0300

    grainger

--- WebScraping/Upwork/Ahmad/active-jobs/Milestone25/grainger.py
+++ WebScraping/Upwork/Ahmad/active-jobs/Milestone25/grainger.py
@@ -7,14 +7,17 @@ from selenium.webdriver.common.keys import Keys
 import time, os, random
 from rich import print
 import pandas as pd
+import warnings
+
+warnings.filterwarnings("ignore")
 
 def parse_documents(driver, xpath, df, index):
     documents = get_elements(driver, xpath, multiple=True)
 
-    manual_key = ['Manual', 'Operating Manual', 'Parts Diagram', 'OIPM', 'Assembly Instructions', 'User Guide']
+    manual_key = ['Manual', 'Operating Manual', 'Parts Diagram', 'OIPM', 'Assembly Instructions', 'User Guide', 'Combo Guide']
     brochure_key = ['Sell sheet', 'Selection Guide']
     specssheet_key = ['Specification Sheet', 'Technical Data Sheet', 'Tech Sheet', 'Datasheet']
-    #docs = {}
+
     if documents:
         for document in documents:
             doc_name = document.text
@@ -32,7 +35,7 @@ def parse_documents(driver, xpath, df, index):
     return df
 
 def parse_details(driver, xpath, df, index):
-    #Keys
+    #Data Points Keys
     amp_keys = ['Amperage', 'current', 'Amps AC', 'Amps']
     volt_keys = ['Voltage', 'Operating Voltage', 'Rated Voltage', 'Output Amplitude']
     watt_keys = ['Power Output', 'Power Consumption', 'Cooking Wattage', 'Wattage', 'Watts']
@@ -40,11 +43,16 @@ def parse_details(driver, xpath, df, index):
     hertz_keys = ['Frequency', 'Hz']
     plug_type_keys = ['Plug Type']
     weight_keys = ['Weight', 'Tool Weight', 'Table Weight']
-    depth_keys = ['Overall Depth', 'Depth',  'Housing Sz (H x W x D)', 'Handle Length', 'Overall Length', 'Table Length', 'Body Depth', 'Tool Length', 'Outside Depth', 'Base Length']
-    height_keys = ['Overall Height', 'Width', 'Width/Diameter', 'Housing Sz (H x W x D)', 'Overall Fixed Height', 'Table Height', 'Body Height', 'Outside Height', 'Stowed Height', 'Overall Height - Maximum']
-    width_keys = ['Overall Width/ Height/ Housing Sz (H x W x D)/ Table Width/Body Width/ Outside Width/ Base Width']
     ada_compliant_keys = ['ADA Compliant', 'ADA Compliance']
     antimicrobial_coating_keys=	['Antimicrobial', 'Features']
+
+    depth_keys = ['Overall Depth', 'Depth', 'Handle Length', 'Overall Length', 'Table Length', 'Body Depth', 'Tool Length', 'Outside Depth',
+                   'Exterior Depth', 'Base Length', 'Housing Sz (H x W x D)']
+    
+    height_keys = ['Overall Height', 'Width/Diameter', 'Overall Fixed Height', 'Table Height', 'Body Height', 'Outside Height', 'Stowed Height',
+                    'Overall Height - Maximum', 'Minimum Adjustable Height','Outside Height', 'Exterior Height', 'Height', 'Housing Sz (H x W x D)']
+    
+    width_keys = ['Overall Width', 'Table Width', 'Body Width', 'Outside Width', 'Base Width', 'Outside Width', 'Exterior Width', 'Width', 'Housing Sz (H x W x D)']
 
     #Get details again due to stale element reference
     details = get_elements(driver, xpath, multiple=True)
@@ -58,7 +66,6 @@ def parse_details(driver, xpath, df, index):
 
             elif any(volt_key.lower() in key.lower() for volt_key in volt_keys) :
                 df.at[index, 'volts'] = str(value)
-            
 
             elif any(watt_key.lower() in key.lower() for watt_key in watt_keys) :
                 df.at[index, 'watts'] = str(value)
@@ -66,45 +73,40 @@ def parse_details(driver, xpath, df, index):
             elif any(phase_key.lower() in key.lower() for phase_key in phase_keys) :
                 df.at[index, 'phase'] = str(value)
             
-            
             elif any(hertz_key.lower() in key.lower() for hertz_key in hertz_keys) :
                 df.at[index, 'hertz'] = str(value)
 
             elif any(plug_type_key.lower() in key.lower() for plug_type_key in plug_type_keys) :
                 df.at[index, 'plug_type'] = str(value)
 
-            elif any(weight_key.lower() in key.lower() for weight_key in weight_keys) :
+            elif any(weight_key.lower() == key.lower() for weight_key in weight_keys) :
                 df.at[index, 'weight'] = str(value)
             
-            
-            elif any(depth_key.lower() in key.lower() for depth_key in depth_keys) :
+            elif any(depth_key.lower() == key.lower() for depth_key in depth_keys) :
                 if 'x' in value:
                     value = value.split('x')[-1]
                     df.at[index, 'depth'] = str(value)
                 else:
                     df.at[index, 'depth'] = str(value)
 
-            elif any(height_key.lower() in key.lower() for height_key in height_keys) :
+            elif any(height_key.lower() == key.lower() for height_key in height_keys) :
                 if 'x' in value:
                     value = value.split('x')[0]
                     df.at[index, 'height'] = str(value)
                 else:
                     df.at[index, 'height'] = str(value)
-           
-            
-            elif any(width_key.lower() in key.lower() for width_key in width_keys) :
+                       
+            elif any(width_key.lower() == key.lower() for width_key in width_keys) :
                 if 'x' in value:
                     value = value.split('x')[1]
                     df.at[index, 'width'] = str(value)
                 else:
                     df.at[index, 'width'] = str(value)
-            
-            
+                    
             elif any(ada_key.lower() in key.lower() for ada_key in ada_compliant_keys) :
                 if value =='No' or value == 'Non-ADA Compliant':
                     df.at[index, 'ada compliant (Y/N)'] = 'N'
-            
-            
+                        
             elif any(ac_key.lower() in key.lower() for ac_key in antimicrobial_coating_keys) :
                 if value.lower == 'no':
                     df.at[index, 'antimicrobial coating (Y/N)'] = 'N'
@@ -125,8 +127,8 @@ def get_elements(driver, xpath, multiple, timeout=4):
 
 def search_items(driver, model_number, df, index, excel_filename):
     searchbar = driver.find_element(By.XPATH, '//div/input[@aria-label="Search Query"]')
-    max_retries = 2
-    retry_delay = 1
+    max_retries = 3
+    retry_delay = random.uniform(1.0, 3.0)
 
     try:   
         retries = 0
@@ -137,9 +139,13 @@ def search_items(driver, model_number, df, index, excel_filename):
                 searchbar.send_keys(model_number) #Type in the Model-Number 
                 searchbar.send_keys(Keys.RETURN) #Hit ENTER 
                 time.sleep(3)
+
+
+                item_url = driver.current_url
+                print(f'[green]Extracting data from: [/green][yellow]{model_number}[/yellow]  >>>  [cyan]URL [/cyan]: {item_url}')
+
                 
                 #Fetch Data Points
-                item_url = driver.current_url
                 image = get_elements(driver, '//div[@data-testid="product-image-to-zoom"]/img', multiple=False)
                 shipping_weight = get_elements(driver, '//div[@data-testid="shipping-weight"]/strong', multiple=False)
 
@@ -202,20 +208,24 @@ def main():
 
 
         count = 0
-        MAX_REQUESTS = 2
-        BASE_SLEEP_TIME = 180
+        MAX_REQUESTS = random.randint(12, 18)
+        BASE_SLEEP_TIME = 60
 
-        df = pd.read_excel(filepath, sheet_name='Master')
-        for index, row in df.head(3).iterrows():
+        df = pd.read_excel(filepath, sheet_name='Master', dtype='str')
+        for index, row in df.iterrows():
             model_number = row['mfr number']
             if '/' in model_number:
                 model_number = model_number.split('/')[0]
+
+            if model_number == '500-030':
+                model_number = '38NT18'
+
             search_items(driver, model_number, df, index, excel_filename)
             count += 1
             time.sleep(2)
 
             if count >= MAX_REQUESTS:
-                sleep_time = BASE_SLEEP_TIME + random.randint(-30, 30) + (count % 10)
+                sleep_time = max(10, BASE_SLEEP_TIME + random.uniform(-30, 30) + (count % 10))
                 print(f"Rate limiting: sleeping for {sleep_time} seconds...")
                 time.sleep(sleep_time) # sleep for four minutes to avoid rate limiting
                 count = 0
