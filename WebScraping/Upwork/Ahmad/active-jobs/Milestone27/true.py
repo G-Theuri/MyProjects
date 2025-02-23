a42370dc4efc154e2003eaa92f0d8d3465ce9c0e
G-Theuri <t.gichukierastus@gmail.com>
2025-02-23 21:19:04 +0300

    true

--- WebScraping/Upwork/Ahmad/active-jobs/Milestone27/true.py
+++ WebScraping/Upwork/Ahmad/active-jobs/Milestone27/true.py
@@ -2,23 +2,98 @@ import pdfplumber
 import fitz #pyMuPDF
 import tabula
 import re
-
+import camelot
+import pandas as pd
+from rich import print
 
 #pdf_path = "resources/TRUE SPEC SHEET.pdf"
 pdf_path = "resources/Hatco Spec Sheet.pdf"
 
-doc = fitz.open(pdf_path)
-full_text = ""
+# doc = fitz.open(pdf_path)
+# full_text = ""
+ 
+# for page_num in range(len(doc)):
+#     page = doc.load_page(page_num)
+#     full_text += page.get_text()
 
-for page_num in range(len(doc)):
-    page = doc.load_page(page_num)
-    full_text = page.
+#     print(full_text)
+
+    
 # with pdfplumber.open('resources\TRUE SPEC SHEET.pdf') as hatco:
 #     text = ""
 #     for page in hatco.pages:
 #         text += page.extract_text()
-
 #         print(text)
 
-dfs = tabula.read_pdf(pdf_path, pages='2')
-print(dfs)
+# tables = tabula.read_pdf(pdf_path, pages="all", multiple_tables = True, lattice=True)
+# print(tables[0])
+
+# with pdfplumber.open(pdf_path) as pdf:
+#     page= pdf.pages[0]
+#     table = page.extract_table()
+
+#     for row in table:
+#         print(row)
+
+# df.columns = ['Model', 'Description', 'Dimensions (W x D x H)', 'Volts', 'Watts', 'Amps', 'Plug', 'Approx. Ship Weight']
+# df.dropna(how='all', inplace=True)
+# df.reset_index(drop=True, inplace=True)
+# print(df.head())
+
+# print(df[df.columns[3]])
+
+tables = camelot.read_pdf(pdf_path, pages='2', flavor='stream')
+
+df =  tables[0].df
+df_clean = df.dropna(how='all', axis=0).dropna(how='all', axis=1)
+df_clean = df_clean.iloc[4:]#.reset_index(drop=True)
+
+data_points = []
+model_names = []
+current_data = []
+for index, row in df_clean.iterrows():
+    if 'NEMA 5-15P' in str(row[7]):
+        if current_data:
+            data_points.append(current_data)
+
+
+    current_data = {
+        'Model': row[0],
+        'Description': row[0],
+        'Dimensions': row[0],
+        'Voltage': row[0],
+        'Watts': row[0],
+        'Amps': row[0],
+        'Plug': row[0],
+        'Shipping Weight': row[0],
+    }
+    if current_data:
+        if pd.notna(row[1]):
+            current_data['Description'] += row[1]# + " "
+
+        if pd.notna(row[2]):
+            current_data['Dimensions'] += row[2]# + " "
+
+        if pd.notna(row[3]):
+            current_data['Voltage'] += row[3]# + " "
+
+        if pd.notna(row[4]):
+            current_data['Watts'] += row[4]# + " "
+
+        if pd.notna(row[6]):
+            current_data['Amps'] += row[6]# + " "
+
+        if pd.notna(row[7]):
+            current_data['Plug'] += row[7]# + " "
+
+        if pd.notna(row[8]):
+            current_data['Shipping Weight'] = row[8]# + " "
+
+
+        data_points.append(current_data)
+
+df_data_points = pd.DataFrame(data_points)
+print(df_clean)
+print(data_points)
+
+
--- test.py
+++ test.py
@@ -7,44 +7,23 @@ import time, re
 from rich import print
 
 chrome_options = uc.ChromeOptions()
-chrome_options.add_argument("--headless")  # Run Chrome in headless mode
+#chrome_options.add_argument("--headless")  # Run Chrome in headless mode
 driver = uc.Chrome(options=chrome_options)
 driver.maximize_window()
 
-page_url = 'https://www.mavinfurniture.com/products/cases/kiawah/'
+page_url = 'https://www.realtor.com/realestateagents/dallas_tx/photo-1/pg-1'
 
 driver.get(page_url)
-time.sleep(2)
+time.sleep(20)
 
 data = {}
 try:
-    images = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.XPATH, '//div[@class="shop ac-cards"]/a[@title]')))
-    for i in images:
-        title = i.get_attribute('title')
-        image = i.get_attribute('href')
-        data[image] = {'caption':title}
-
+    listings = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.XPATH, '//*[@id="agent_list_wrapper"]/div[1]/ul/div')))
+    next_page = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="agent_list_wrapper"]/div[2]/div/a[2]')))
+    if next_page:
+        print(len(listings))
     
-except Exception as e:
-    print('Images not found', e)
-
-try:
-    for image, info in data.items():
-        # Regex to extract SKU (like ADM1902, ADM1904, etc.)
-        sku_match = re.search(r'\b[A-Za-z]{3}\d{4}\b', info['caption'])
-        sku = sku_match.group(0) if sku_match else None  # If no SKU found, set to None
-        info['sku'] = sku
-        additional_info = info['caption'] if not sku else None
-        info['Additional Info'] = additional_info
-        col_image = image if additional_info else None
-        info['Collection Image'] = col_image
-
-    #print(f'[green]Succesfully Got Dynamic Content from:[/green] {page_url}')
-    print(data)
-except Exception as e:
-    print('[red]Unsuccessful![/red]', e)
-
-    
-
-finally:
-    driver.quit()
+except:
+    pass
+time.sleep(2)
+driver.quit()
